07548a1d79071446c4c092e53d9d1c47d13929a0
Larissa Nussbaumer <larissa_nussbaumer@gmx.ch>
2021-11-15 10:27:44 +0100

    render-Funktion in der Klasse ViewModel

--- dice3.js
+++ dice3.js
@@ -20,7 +20,7 @@ class Button{
         this.button.onclick = function(){
             randomNumberGenerator.perform()
             this.zufallszahl = randomNumberGenerator.getValue()
-            render()
+            viewModel.render()
         }
     }
 
@@ -54,25 +54,28 @@ class TextBlock{
     }
 }
 
-
-function render(){
-    var body = document.getElementsByTagName("body")[0]
-    var button1 = document.getElementsByTagName("button")[0]
-    if(button1){
-        body.removeChild(button1)
+class ViewModel{
+    
+    render(){
+        var body = document.getElementsByTagName("body")[0]
+        var button1 = document.getElementsByTagName("button")[0]
+        if(button1){
+            body.removeChild(button1)
+        }
+        button1 = new Button()
+        button1.buttonText = 'wuerfeln'
+        var textBlock1 = document.getElementsByTagName("div")[0]
+        if(textBlock1){
+            body.removeChild(textBlock1)
+        }
+        textBlock1 = new TextBlock()
+        textBlock1.text = `${randomNumberGenerator.getValue()}`    
+        body.appendChild(button1.buttonElement)
+        body.appendChild(textBlock1.textElement)
     }
-    button1 = new Button
-    button1.buttonText = 'wuerfeln'
-    var textBlock1 = document.getElementsByTagName("div")[0]
-    if(textBlock1){
-        body.removeChild(textBlock1)
-    }
-    textBlock1 = new TextBlock()
-    textBlock1.text = `${randomNumberGenerator.getValue()}`    
-    body.appendChild(button1.buttonElement)
-    body.appendChild(textBlock1.textElement)
 }
 
 
 var randomNumberGenerator = new RandomNumberGenerator(1,6)
-render()
+var viewModel = new ViewModel()
+viewModel.render()
